1f4b772791b7b4d7e0c7291dec05752b113dfd81
joaquincodigo <joaquin.codigo@gmail.com>
2024-11-25 17:20:31 -0300

    fixed top spacing

--- app/components/Title.js
+++ app/components/Title.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Title() {
   return (
-    <div className="title-wrapper text-center mb-8">
+    <div className="title-wrapper text-center mb-8 mt-4">
       <h1 className="text-4xl font-semibold inline-block text-slate-800">
         Simple Tasker
       </h1>
